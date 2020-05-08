f852a34e26b7e146c0cc3fba71d512c38b8074bc
Steen RH <steen.rickmer@gmx.de>
2020-05-08 19:13:26 +0200

    fixed kingfisher set body sprite; fixed rookie set; fixed iron core sprite scaling

---
+++ Items/Vanity/RookieSet/RookieBase.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace JourneyTrend.Items.Vanity.RookieSet
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class RookieBase : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Base");
+            Tooltip.SetDefault("Stay Grounded.");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.rare = 1;
+            item.vanity = true;
+        }
+        public override bool DrawLegs()
+        {
+            return false;
+        }
+    }
+}
