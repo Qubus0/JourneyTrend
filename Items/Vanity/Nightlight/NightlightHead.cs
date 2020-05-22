c7daa8dfaa283d773d5c576913af86b4bba8b8be
Steen RH <steen.rickmer@gmx.de>
2020-05-22 14:03:36 +0200

    added nightlight set (glow missing) added icon

---
+++ Items/Vanity/Nightlight/NightlightHead.cs
@@ -0,0 +1,33 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace JourneyTrend.Items.Vanity.Nightlight
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class NightlightHead : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nightlight Eyes");
+            Tooltip.SetDefault("Eyes that reflect a moonlit night...\nMade by Metalsquirrel.");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.rare = 2;
+            item.vanity = true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddTile(TileID.Loom);
+            recipe.AddIngredient(ItemID.Silk, 15);
+            recipe.AddIngredient(ItemID.Moonglow, 5);
+            recipe.AddIngredient(ItemID.Lens, 2);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
