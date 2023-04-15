27149412a48af62d29f9802cd506e0a41bdf9801
Qubus0 <steen.rickmer@gmx.de>
2023-04-15 17:31:21 +0200

    add shadow fiend glow

--- Items/Vanity/ShadowFiend/ShadowFiendBody.cs
+++ Items/Vanity/ShadowFiend/ShadowFiendBody.cs
@@ -1,3 +1,8 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,10 +11,24 @@ namespace JourneyTrend.Items.Vanity.ShadowFiend
     [AutoloadEquip(EquipType.Body)]
     public class ShadowFiendBody : ModItem
     {
+        private static Lazy<Asset<Texture2D>> Glowmask;
+
+        public override void Unload()
+        {
+            Glowmask = null;
+        }
+
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Shadow Fiend Breastplate");
             Tooltip.SetDefault("Cursed Flames consume you!\nMade by CakeBoiii");
+
+            if (!Main.dedServ)
+            {
+                Glowmask = new(() => ModContent.Request<Texture2D>(Texture + "Glow"));
+
+                BodyGlowmaskPlayer.RegisterData(Item.bodySlot, () => new Color(255, 255, 255, 0));
+            }
         }
 
         public override void SetDefaults()
@@ -23,9 +42,9 @@ namespace JourneyTrend.Items.Vanity.ShadowFiend
         public override void AddRecipes()
         {
             CreateRecipe()
-            .AddTile<Tiles.SewingMachine>()
-            .AddRecipeGroup("JourneyTrend:WorldEvilDemonBodies")
-            .Register();
+                .AddTile<Tiles.SewingMachine>()
+                .AddRecipeGroup("JourneyTrend:WorldEvilDemonBodies")
+                .Register();
         }
     }
 }
